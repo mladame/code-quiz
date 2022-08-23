4287b9393ad385a80fc327ca6a979162a41acfc4
Martha A <mladame43@gmail.com>
2022-08-23 00:24:16 -0700

    outline html, js notes, css for html

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Define elements
+
+// Question pool (array)
+
+// init function
+
+// start game function
+
+// win game condition
+
+// lose game function
+
+// start timer funtion
+
+// render question and answers
+
+// update high score/initials and save to local storage
+
+// event listeners start game
+
+// event listener click "true" button, if else
+
+// reset game/play again
