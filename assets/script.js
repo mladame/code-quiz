2a8d97feb80c11afeef63bb21f237d0df7da98b9
Martha A <mladame43@gmail.com>
2022-08-24 22:37:30 -0700

    nextQuestion working, question loading js, updated id's and classes

--- assets/script.js
+++ assets/script.js
@@ -2,115 +2,151 @@
 const startPage = document.getElementById('start-game');
 const startButton = document.getElementById('start-btn');
 var questionContainer = document.getElementById('question-container');
-var answerButtons = document.getElementById('answer-btn');
+var quizQuestion = document.getElementById('quiz-question');
+// var answerButtons = document.getElementById('answer-btn');
+var answerBTNa = document.getElementById('answer-btn-a');
+var answerBTNb = document.getElementById('answer-btn-b');
+var answerBTNc = document.getElementById('answer-btn-c');
+var answerBTNd = document.getElementById('answer-btn-d');
 var signScore = document.getElementById('sign-score');
 var viewHighscore = document.getElementById('view-highscores');
 var score = 0;
 var highscore = localStorage.getItem("highscore");
 
 // startPage.setAttribute('hidden', "false");
-questionContainer.setAttribute('hidden', true);
-signScore.setAttribute('hidden', true);
-viewHighscore.setAttribute('hidden', true);
+questionContainer.setAttribute('style', 'display:none');
+signScore.setAttribute('style', 'display: none;');
+viewHighscore.setAttribute('style', 'display: none;');
 
 // Question pool (array)
 var questions = [
     {
-        title: 'Which of the following is an HTML semantic element?',
-        answers: [
-            'span',
-            'article',
-            'class',
-            'div'
-        ],
+        question: 'Which of the following is an HTML semantic element?',
+        answerA: ['span'],
+        answerB: ['article'],
+        answerC: ['class'],
+        answerD: ['div'],
+        // answers: [
+        //     ,
+        //     ',
+        //     ,
+        // ],
         correct: 'article'
     },
     {
-        title: 'Which of the following shows the correct way to write class="Code-Mania"?',
-        answers: [
-            '#Code-Mania',
-            '#code-mania',
-            '.Code-Mania',
-            'Code-Mania'
-        ],
+        question: 'Which of the following shows the correct way to write class="Code-Mania"?',
+        answerA: ['#Code-Mania'],
+        answerB: ['#code-mania'],
+        answerC: ['.Code-Mania'],
+        answerD: ['Code-Mania'],
+        // answers: [
+        //     ,
+        //     ,
+        //     ,
+            
+        // ],
         correct: '.Code-Mania'
     },
     {
-        title: 'What is the purpose of a Boolean() function?',
-        answers: [
-            'to find out if an expression is true',
-            'to hold a list of values',
-            'to run the same code over and over again',
-            'to hold key-value pairs'
-        ],
+        question: 'What is the purpose of a Boolean() function?',
+        answerA: ['to find out if an expression is true'],
+        answerB: ['to hold a list of values'],
+        answerC: ['to run the same code over and over again'],
+        answerD: ['to hold key-value pairs'],
+        // answers: [
+        //     ,
+        //     ,
+        //     ,
+            
+        // ],
         correct: 'to find out if an expression is true'
     },
     {
-        title: 'Which of the following is not a part of the CSS Box Model?',
-        answers: [
-            'margin',
-            'card',
-            'padding',
-            'border'
-        ],
+        question: 'Which of the following is not a part of the CSS Box Model?',
+        answerA: ['margin'],
+        answerB: ['card'],
+        answerC: ['padding'],
+        answerD: ['border'],
+        // answers: [
+        //     ,
+        //     ,
+        //     ,
+            
+        // ],
         correct: 'card'
     },
     {
-        title: 'Where, in an HTML sheet, should a css stylesheet be linked?',
-        answers: [
-            'header',
-            'body',
-            'footer',
-            'head'
-        ],
+        question: 'Where, in an HTML sheet, should a css stylesheet be linked?',
+        answerA: ['header'],
+        answerB: ['body'],
+        answerC: ['footer'],
+        answerD: ['head'],
+        // answers: [
+        //     ,
+        //     ,
+        //     ,
+            
+        // ],
         correct: 'head'
     },
     {
-        title: 'Which Javascript assignment operator assigns a remainder to a variable?',
-        answers: [
-            '+=',
-            '/=',
-            '-=',
-            '%='
-        ],
+        question: 'Which Javascript assignment operator assigns a remainder to a variable?',
+        answerA: ['+='],
+        answerB: ['/='],
+        answerC: ['-='],
+        answerD: ['%='],
+        // answers: [
+        //     ,
+        //     ,
+        //     ,
+            
+        // ],
         correct: '%='
     }
 ]
 
 // init function (might not need)
 
-// event listeners start game
+// Start Game
 startButton.addEventListener("click", startGame);
 
-// start game function
 function startGame() {
-    // console.log('started');
-    startPage.setAttribute("hidden", true)
-    // startPage.classList.add('hide')
+    startPage.setAttribute('style', 'display: none;')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-
-    // questionContainer.classList.remove('hide')
-    // startPage.style.display = 'none';
     nextQuestion()
     // startTimer()
     // need a getscore() function that tracks true/false/correct responses
-}
-
-function nextQuestion(){
-    console.log("questions should appear");
-
-    questionContainer.setAttribute('hidden', false)
-    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function startTimer(){
 
 }
 
+function nextQuestion(){
+    console.log("questions should appear");
+
+    questionContainer.setAttribute('style', 'display: block;')
+
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
 function showQuestion(){
     console.log('question here');
+    quizQuestion.textContent=questions[currentQuestionIndex].question;
+    // answerButtons.textContent=questions[currentQuestionIndex].answers;
+    // quizQuestion.innerText=questions.question;
+    answerBTNa.textContent=questions[currentQuestionIndex].answerA;
+    answerBTNb.textContent=questions[currentQuestionIndex].answerB;
+    answerBTNc.textContent=questions[currentQuestionIndex].answerC;
+    answerBTNd.textContent=questions[currentQuestionIndex].answerD;
 }
+
+// // Add up correct answers
+// answerButtons.addEventListener('click', selectAnswer());
+// function selectAnswer() {
+
+// }
 
 
 // win game condition
